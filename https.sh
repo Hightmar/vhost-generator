72f9a6c155bb962b90ef0b6438bc8733d7d752a8
Antoine <antoine.lounis@protonmail.com>
2022-03-31 20:39:47 +0200

    Syntax modification & comments added
     According to SHELLCHECK recommendations

--- https.sh
+++ https.sh
@@ -3,25 +3,25 @@
 apacheSuffix=apache.conf
 nginxSuffix=nginx.conf
 
-pathSiteAvailabeNginx="/etc/nginx/sites-available"
-pathSiteAvailabeApache="/etc/apache2/sites-available"
+pathSiteAvailableNginx="/etc/nginx/sites-available"
+pathSiteAvailableApache="/etc/apache2/sites-available"
 
 echo "################################################"
 echo "##    CERTIFICATES LET'S ENCRYPT CREATED ?    ##"
 echo "################################################"
 
 while true; do
-    read  -p "Certificates generated ? (y/n) " cerbotUsed
-    case $cerbotUsed in
-        [Yy]* ) cerbotUsed="y" break;;
-        [Nn]* ) cerbotUsed="n" break;;
+    read  -rp "Certificates generated ? (y/n) " certbotUsed
+    case $certbotUsed in
+        [Yy]* ) certbotUsed="y" break;;
+        [Nn]* ) certbotUsed="n" break;;
         * ) echo "Yes, y/No, n.";;
     esac
 done
 
-if [ $cerbotUsed == "y" ]; then
+if [ "$certbotUsed" == "y" ]; then
     while true; do
-        read -p "Activate on Apache or Nginx ? " servWeb
+        read -rp "Activate on Apache or Nginx ? " servWeb
         case $servWeb in
             "apache" ) servWeb="apache" break;;
             "nginx" ) servWeb="nginx" break;;
@@ -29,10 +29,10 @@ if [ $cerbotUsed == "y" ]; then
         esac
     done
 
-    read -p "Which vHost to edit ? (name without suffix) " siteName
+    read -rp "Which vHost to edit ? (name without suffix) " siteName
 
     while true; do
-        read -p "Activate HSTS ? (y/n) " hsts
+        read -rp "Activate HSTS ? (y/n) " hsts
         case $hsts in
             [Yy]* ) hsts="y" break;;
             [Nn]* ) hsts="n" break;;
@@ -44,106 +44,119 @@ if [ $cerbotUsed == "y" ]; then
     # APACHE2
     ##
 
-    if [ $servWeb == "apache" ]; then
-      vHostEdit=$pathSiteAvailabeApache/$siteName.$apacheSuffix
-      source generated_vhost/$vHostEdit.variables
+    if [ "$servWeb" == "apache" ]; then
+      vHostEdit=$pathSiteAvailableApache/$siteName.$apacheSuffix
+      source generated_vhost/"$vHostEdit".variables
 
-      sed -i '/\/VirtualHost/d' $vHostEdit
-      sed -i '/#deleteifhttps/d' $vHostEdit
+      sed -i '/\/VirtualHost/d' "$vHostEdit"
+      sed -i '/#deleteifhttps/d' "$vHostEdit"
 
-      cat base_vhost/apache/ssl >> $vHostEdit
+      # ADD SSL BLOCK
+      cat base_vhost/apache/ssl >> "$vHostEdit"
 
-      sed -i 's/${serverName}/'$serverName'/' $vHostEdit
-      sed -i 's/${aliasName}/'$aliasName'/' $vHostEdit
-      sed -i 's~${documentRoot}~'$documentRoot'~' $vHostEdit
+      sed -i 's/${serverName}/'"$serverName"'/' "$vHostEdit"
+      sed -i 's/${aliasName}/'"$aliasName"'/' "$vHostEdit"
+      sed -i 's~${documentRoot}~'"$documentRoot"'~' "$vHostEdit"
 
-      if [ $hsts == "y" ]; then
-        sed -i 's/#hsts//' $vHostEdit
+      if [ "$hsts" == "y" ]; then
+        sed -i 's/#hsts//' "$vHostEdit"
       else
-        sed -i '/#hsts/d' $vHostEdit
+        sed -i '/#hsts/d' "$vHostEdit"
       fi
 
       sudo systemctl restart apache2
       echo "vhost updated to handle HTTPS on Apache"
+
+      # DELETE FILE WITH VARIABLES
+      rm generated_vhost/"$vHostEdit".variables
     fi
 
     ##
     # NGINX
     ##
 
-    if [ $servWeb == "nginx" ]; then
-      vHostEdit=$pathSiteAvailabeNginx/$siteName.$nginxSuffix
-      source generated_vhost/$vHostEdit.variables
+    if [ "$servWeb" == "nginx" ]; then
+      vHostEdit=$pathSiteAvailableNginx/$siteName.$nginxSuffix
+      source generated_vhost/"$vHostEdit".variables
 
-      cat base_vhost/nginx/ssl >> $vHostEdit
+      # ADD SSL BLOCK
+      cat base_vhost/nginx/ssl >> "$vHostEdit"
 
-      sed -i 's/${serverName}/'$serverName'/' $vHostEdit
-      sed -i 's/${aliasName}/'$aliasName'/' $vHostEdit
-      sed -i 's~${documentRoot}~'$documentRoot'~' $vHostEdit
+      sed -i 's/${serverName}/'"$serverName"'/' "$vHostEdit"
+      sed -i 's/${aliasName}/'"$aliasName"'/' "$vHostEdit"
+      sed -i 's~${documentRoot}~'"$documentRoot"'~' "$vHostEdit"
 
-      if [ $hsts == "y" ]; then
-        sed -i 's/#hsts//' $vHostEdit
+      if [ "$hsts" == "y" ]; then
+        sed -i 's/#hsts//' "$vHostEdit"
       else
-        sed -i '/#hsts/d' $vHostEdit
+        sed -i '/#hsts/d' "$vHostEdit"
       fi
 
-      phpUsed=$(cat $vHostEdit | grep fastcgi_pass)
+      phpUsed=$(cat "$vHostEdit" | grep fastcgi_pass)
 
       if [ -n "$phpUsed" ]; then
-        sed -i '/#deleteifhttps/d' $vHostEdit
-        sed -i '/#end/d' $vHostEdit
-        cat base_vhost/nginx/php >> $vHostEdit
-        sed -i 's/${phpVersion}/'$phpVersion'/' $vHostEdit
-        sed -i 's/ #deleteifhttps//' $vHostEdit
+        sed -i '/#deleteifhttps/d' "$vHostEdit"
+        sed -i '/#end/d' "$vHostEdit"
+
+        # ADD PHP BLOCK
+        cat base_vhost/nginx/php >> "$vHostEdit"
+
+        sed -i 's/${phpVersion}/'"$phpVersion"'/' "$vHostEdit"
+        sed -i 's/ #deleteifhttps//' "$vHostEdit"
 
       elif [ -z "$phpUsed" ]; then
-        phpUsed=$(cat $vHostEdit | grep proxy_pass)
+        phpUsed=$(cat "$vHostEdit" | grep proxy_pass)
 
         if [ -n "$phpUsed" ]; then
-          sed -i '/#deleteifhttps/d' $vHostEdit
-          sed -i '/#end/d' $vHostEdit
+          sed -i '/#deleteifhttps/d' "$vHostEdit"
+          sed -i '/#end/d' "$vHostEdit"
+
+          # ADD REVERSE_PROXY BLOCK
           cat base_vhost/nginx/phpreverse >> "$vHostEdit"
-          sed -i 's/${ipToSend}/'$ipToSend'/' "$vHostEdit"
-          sed -i 's/${portToSend}/'$portToSend'/' "$vHostEdit"
-          sed -i 's/ #deleteifhttps//' $vHostEdit
+
+          sed -i 's/${ipToSend}/'"$ipToSend"'/' "$vHostEdit"
+          sed -i 's/${portToSend}/'"$portToSend"'/' "$vHostEdit"
+          sed -i 's/ #deleteifhttps//' "$vHostEdit"
         fi
       fi
 
-      sudo systemctl restard nginx
+      sudo systemctl restart nginx
       echo "vhost updated to handle HTTPS on NGINX"
-      rm generated_vhost/$vHostEdit.variables
+
+      # DELETE FILE WITH VARIABLES
+      rm generated_vhost/"$vHostEdit".variables
     fi
 
 else
-    echo "Create Let's Encrypt certificates first (see CertBot)"
+  echo "Create Let's Encrypt certificates first (see CertBot)"
+  while true; do
+      read -rp "Start Certbot ? (y/n)" certbot
+      case $certbot in
+          [Yy]* ) certbot="y" break;;
+          [Nn]* ) certbot="n" break;;
+          * ) echo "Yes, y/No, n.";;
+      esac
+  done
+
+  if [ "$certbot" == "y" ]; then
     while true; do
-        read -p "Start Cerbot ? (y/n)" certbot
-        case $certbot in
-            [Yy]* ) certbot="y" break;;
-            [Nn]* ) cerbot="n" break;;
-            * ) echo "Yes, y/No, n.";;
-        esac
+      read -rp "Which webserver ? (apache/nginx/exit) " certServWeb
+      case $certServWeb in
+          "apache" ) certServWeb="apache" break;;
+          "nginx" ) certServWeb="nginx" break;;
+          "exit" ) exit;;
+          * ) echo "apache or nginx";;
+      esac
     done
 
-    if [ $certbot == "y" ]; then
-      while true; do
-        read -p "Which webserver ? (apache/nginx/exit) " certServWeb
-        case $certServWeb in
-            "apache" ) certServWeb="apache" break;;
-            "nginx" ) certServWeb="nginx" break;;
-            "exit" ) exit;;
-            * ) echo "apache or nginx";;
-        esac
-      done
-
-      if [ $certServWeb == "apache" ]; then
-        sudo certbot certonly --apache
-        enableCert=$(readlink -f "$0")
-        exec $enableCert
-      else
-        sudo certbot certonly --nginx
-        enableCert=$(readlink -f "$0")
-        exec $enableCert
-      fi
+    if [ "$certServWeb" == "apache" ]; then
+      sudo certbot certonly --apache
+      enableCert=$(readlink -f "$0")
+      exec $enableCert
+    else
+      sudo certbot certonly --nginx
+      enableCert=$(readlink -f "$0")
+      exec $enableCert
     fi
+  fi
 fi
--- vhost.sh
+++ vhost.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# CREATE DIR IF NOT EXIST
 if [ ! -d generated_vhost ];then
    mkdir generated_vhost
 fi
@@ -7,18 +8,17 @@ fi
 apacheSuffix=apache.conf
 nginxSuffix=nginx.conf
 
-pathSiteAvailabeNginx="/etc/nginx/sites-available"
-pathSiteAvailabeApache="/etc/apache2/sites-available"
+pathSiteAvailableNginx="/etc/nginx/sites-available"
+pathSiteEnabledNginx="/etc/nginx/sites-enabled"
+pathSiteAvailableApache="/etc/apache2/sites-available"
 
-read -p "vHost name (domain): " serverName
-read -p "Alias name: " aliasName
-read -p "Document root (without /var/www/): " documentRoot
+read -rp "vHost name (domain): " serverName
+read -rp "Alias name: " aliasName
+read -rp "Document root (without /var/www/): " documentRoot
 documentRoot="/var/www/"$documentRoot
 
-
-
 while true; do
-      read -p "Create on Apache  ? " doApache
+      read -rp "Create on Apache  ? " doApache
       case $doApache in
           [Yy] ) doApache="y" break;;
           [Nn] ) doApache="n" break;;
@@ -26,14 +26,15 @@ while true; do
       esac
 done
 
-if [ $doApache == "y" ]; then
+# BLOCK FOR APACHE2'S VHOST
+if [ "$doApache" == "y" ]; then
 
-  vHostApache=$pathSiteAvailabeApache/$serverName.$apacheSuffix
-  touch $vHostApache
-  cat base_vhost/apache/base >> $vHostApache
+  vHostApache=$pathSiteAvailableApache/$serverName.$apacheSuffix
+  touch "$vHostApache"
+  cat base_vhost/apache/base >> "$vHostApache"
 
   while true; do
-     read -p "Used with reverse proxy ? " reverse
+     read -rp "Used with reverse proxy ? " reverse
      case $reverse in
          [Yy]* ) reverse="y" break;;
          [Nn]* ) reverse="n" break;;
@@ -42,33 +43,39 @@ if [ $doApache == "y" ]; then
   done
 
   #If Apache is used with a reverse_proxy
-  if [ $reverse == "y" ]; then
-    read -p "Port used by Apache : " portUsed
-    sed -i 's/${portUsed}/'$portUsed'/' $vHostApache
+  if [ "$reverse" == "y" ]; then
+    read -rp "Port used by Apache : " portUsed
+    sed -i 's/${portUsed}/'"$portUsed"'/' "$vHostApache"
 
   else
       portUsed="80"
-      sed -i 's/${portUsed}/'$portUsed'/' $vHostApache
+      sed -i 's/${portUsed}/'$portUsed'/' "$vHostApache"
   fi
 
-  sed -i 's/${serverName}/'$serverName'/' $vHostApache
-  sed -i 's/${aliasName}/'$aliasName'/' $vHostApache
-  sed -i 's~${documentRoot}~'$documentRoot'~' $vHostApache
-  sudo a2ensite $serverName
+  # MODIFY VHOST WITH INFORMATION'S
+  sed -i 's/${serverName}/'"$serverName"'/' "$vHostApache"
+  sed -i 's/${aliasName}/'"$aliasName"'/' "$vHostApache"
+  sed -i 's~${documentRoot}~'"$documentRoot"'~' "$vHostApache"
+  sudo a2ensite "$serverName"
   sudo systemctl restart apache2
   echo "vHost created and enabled on Apache"
 
   #Create files with variable. Will be used for https
-  touch generated_vhost/$vHostApache.variables
-  echo serverName=$serverName >> generated_vhost/$vHostApache.variables
-  echo aliasName=$aliasName >> generated_vhost/$vHostApache.variables
-  echo documentRoot=$documentRoot >> generated_vhost/$vHostApache.variables
-  echo portUsed=$portUsed >> generated_vhost/$vHostApache.variables
+  touch generated_vhost/"$vHostApache".variables
+
+  # WRITE IN FILE
+  {
+    echo serverName="$serverName"
+    echo aliasName="$aliasName"
+    echo documentRoot="$documentRoot"
+    echo portUsed="$portUsed"
+  } >> generated_vhost/"$vHostApache".variables
+  # END WRITE
 
 fi
 
 while true; do
-      read -p "Create on Nginx  ? " doNginx
+      read -rp "Create on Nginx  ? " doNginx
       case $doNginx in
           [Yy] ) doNginx="y" break;;
           [Nn] ) doNginx="n" break;;
@@ -76,14 +83,22 @@ while true; do
       esac
 done
 
-if [ $doNginx == "y" ]; then
+if [ "$doNginx" == "y" ]; then
 
-  vHostNginx=$pathSiteAvailabeNginx/$serverName.$nginxSuffix
-  touch $vHostNginx
-  cat base_vhost/nginx/base >> $vHostNginx
+  vHostNginx=$pathSiteAvailableNginx/$serverName.$nginxSuffix
+  vHostEnabled=$pathSiteEnabledNginx/$serverName.$nginxSuffix
 
+  touch "$vHostNginx"
+  cat base_vhost/nginx/base >> "$vHostNginx"
+
+  # MODIFY VHOST WITH INFORMATION'S
+  sed -i 's/${serverName}/'$serverName'/' "$vHostNginx"
+  sed -i 's/${aliasName}/'$aliasName'/' "$vHostNginx"
+  sed -i 's~${documentRoot}~'"$documentRoot"'~' "$vHostNginx"
+
+  # ASK FOR PHP AND IF IT'S USED AS REVERSE PROXY
   while true; do
-      read -p "Activate PHP ? " activatePHP
+      read -rp "Activate PHP ? " activatePHP
       case $activatePHP in
           [Yy]* ) activatePHP="y" break;;
           [Nn]* ) activatePHP="n" break;;
@@ -91,119 +106,73 @@ if [ $doNginx == "y" ]; then
       esac
     done
 
-    #Ask version of PHP
-    if [ $activatePHP == "y" ]; then
+  if [ "$activatePHP" == "y" ]; then
 
-      while true; do
-        read -p "Nginx is a reverse_proxy ? " reverseProxy
-        case $reverseProxy in
-            [Yy]* ) reverseProxy="y" break;;
-            [Nn]* ) reverseProxy="n" break;;
-            * ) echo "Yes or No";;
-        esac
-      done
+    while true; do
+      read -rp "Nginx is a reverse_proxy ? " reverseProxy
+      case $reverseProxy in
+          [Yy]* ) reverseProxy="y" break;;
+          [Nn]* ) reverseProxy="n" break;;
+          * ) echo "Yes or No";;
+      esac
+    done
 
-    if [ $reverseProxy == "y" ]; then
-      read -p "On which IP to send ? " ipToSend
-      read -p "On which port to send ? " portToSend
+    if [ "$reverseProxy" == "y" ]; then
+      read -rp "On which IP to send ? " ipToSend
+      read -rp "On which port to send ? " portToSend
 
       sed -i '/}/d' "$vHostNginx"
       cat base_vhost/nginx/phpreverse >> "$vHostNginx"
 
-      sed -i 's/${ipToSend}/'$ipToSend'/' "$vHostNginx"
-      sed -i 's/${portToSend}/'$portToSend'/' "$vHostNginx"
+      sed -i 's/${ipToSend}/'"$ipToSend"'/' "$vHostNginx"
+      sed -i 's/${portToSend}/'"$portToSend"'/' "$vHostNginx"
     else
-      read -p "PHP version ? (e.g. 7.4) " phpVersion
+      read -rp "PHP version ? (e.g. 7.4) " phpVersion
 
       sed -i '/}/d' "$vHostNginx"
       cat base_vhost/nginx/php >> "$vHostNginx"
-      sed -i 's/${phpVersion}/'$phpVersion'/' "$vHostNginx"
-    fi
-
-    sed -i 's/${serverName}/'$serverName'/' $vHostNginx
-    sed -i 's/${aliasName}/'$aliasName'/' $vHostNginx
-    sed -i 's~${documentRoot}~'$documentRoot'~' $vHostNginx
-
-#    sudo ln -s $vHostNginx $vHostEnabledNginx
-    sudo systemctl restart nginx
-    echo "vHost created and enabled on NGINX"
-
-    #Create files with variable. Will be used for https
-    touch generated_vhost/"$vHostNginx".variables
-    echo serverName="$serverName" >> generated_vhost/"$vHostNginx".variables
-    echo aliasName="$aliasName" >> generated_vhost/"$vHostNginx".variables
-    echo documentRoot="$documentRoot" >> generated_vhost/"$vHostNginx".variables
-
-    if [ -n "$portToSend" ] && [ -n "$ipToSend" ]; then
-      echo portToSend="$portToSend" >> generated_vhost/"$vHostNginx".variables
-      echo ipToSend="$ipToSend">> generated_vhost/"$vHostNginx".variables
-    fi
-
-    if [ -n "$phpVersion" ]; then
-      echo phpVersion="$phpVersion" >> generated_vhost/"$vHostNginx".variables
+      sed -i 's/${phpVersion}/'"$phpVersion"'/' "$vHostNginx"
     fi
   fi
+  # END PHP
+
+  sudo ln -s "$vHostNginx" "$vHostEnabled"
+  sudo systemctl restart nginx
+  echo "vHost created and enabled on NGINX"
+
+  #Create files with variable. Will be used for https
+  touch generated_vhost/"$vHostNginx".variables
+
+  # WRITE IN FILE
+  {
+    echo serverName="$serverName"
+    echo aliasName="$aliasName"
+    echo documentRoot="$documentRoot"
+  } >> generated_vhost/"$vHostNginx".variables
+
+  if [ -n "$portToSend" ] && [ -n "$ipToSend" ]; then
+    {
+      echo portToSend="$portToSend"
+      echo ipToSend="$ipToSend"
+    } >> generated_vhost/"$vHostNginx".variables
+  fi
+
+  if [ -n "$phpVersion" ]; then
+    echo phpVersion="$phpVersion" >> generated_vhost/"$vHostNginx".variables
+  fi
+  # END WRITE
 fi
-######################################################################
-#if [ $doNginx == "y" ]; then
-#    defaultNginx=000-default-script.$nginxSuffix
-#    vHostNginx=$serverName.$nginxSuffix
-#    vHostEnabledNginx=$serverName.$nginxSuffix
-#
-#    cp $defaultNginx $vHostNginx
-#
-#    while true; do
-#      read -p "Activate PHP ? " activatePHP
-#      case $activatePHP in
-#          [Yy]* ) activatePHP="y" break;;
-#          [Nn]* ) activatePHP="n" break;;
-#          * ) echo "Yes or No";;
-#      esac
-#    done
-#
-#    #Ask version of PHP
-#    if [ $activatePHP == "y" ]; then
-#
-#      while true; do
-#      read -p "Nginx is a reverse_proxy ? " reversePorxy
-#      case $reversePorxy in
-#          [Yy]* ) reversePorxy="y" break;;
-#          [Nn]* ) reversePorxy="n" break;;
-#          * ) echo "Yes or No";;
-#      esac
-#    done
-#
-#    if [ $reversePorxy == "y" ]; then
-#      read -p "portUsed " phpVersion
-#
-#      read -p "PHP version ? (e.g. 7.4) " phpVersion
-#      sed -i 's/${phpVersion}/'$phpVersion'/' $vHostNginx
-#
-#      #Uncomment php block
-#      sed -i 's/#activatephp//' $vHostNginx
-#    else
-#      #If no php, delete php block
-#      sed -i '/#activatephp/d' $vHostNginx
-#    fi
-#
-#    sed -i 's/${serverName}/'$serverName'/' $vHostNginx
-#    sed -i 's/${aliasName}/'$aliasName'/' $vHostNginx
-#    sed -i 's~${documentRoot}~'$documentRoot'~' $vHostNginx
-#    sudo ln -s $vHostNginx $vHostEnabledNginx
-#    sudo systemctl restart nginx
-#    echo "vHost created and enabled on NGINX"
-#fi
 
 while true; do
-      read -p "Create more vHost ? " moreVHost
-      case $moreVHost in
-          [Yy] ) moreVHost="y" break;;
-          [Nn] ) moreVHost="n" break;;
-          * ) echo "Yes or No";;
-      esac
+  read -rp "Create more vHost ? " moreVHost
+  case $moreVHost in
+    [Yy] ) moreVHost="y" break;;
+    [Nn] ) moreVHost="n" break;;
+    * ) echo "Yes or No";;
+    esac
 done
 
-if [ $moreVHost == "y" ]; then
+if [ "$moreVHost" == "y" ]; then
     addMoreVHost=$(readlink -f "$0")
     exec $addMoreVHost
 else
